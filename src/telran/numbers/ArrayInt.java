454d094f8523802326354b623e6b9365f2259ff3
Yuragranovsky <yuragranovsky@gmail.com>
2022-06-23 21:09:18 +0300

    adds CW #4 with TODO's for HW #4

--- src/telran/numbers/ArrayInt.java
+++ src/telran/numbers/ArrayInt.java
@@ -59,4 +59,56 @@ public class ArrayInt {
 		}
 		return res;
 	}
+	public static void sort(int[] ar) {
+		for (int i = 0; i < ar.length; i++) {
+			moveMaxToEnd(ar);
+		}
+		//TODO
+		//Improve algorithm with a proper moveMaxToEnd method call
+		
+	}
+	private static void moveMaxToEnd(int[] ar) {
+		for(int i = 1; i < ar.length; i++) {
+			if (ar[i - 1] > ar[i]) {
+				swap(ar, i);
+			}
+		}
+		//TODO
+		//Improve algorithm of moveMaxToEnd:
+		// (1) no compare of elements that already exist on the proper places
+		// (think of additional parameter of the method with code update)
+		// (2) terminate algorithm once an array is already sorted (think of returning some
+		// value with code update)
+		
+	}
+	private static void swap(int[] ar, int index) {
+		int tmp = ar[index -1];
+		ar[index -1] = ar[index];
+		ar[index] = tmp;
+		
+	}
+	/**
+	 * 
+	 * @param ar - sorted array
+	 * @param number
+	 * @return if the given number exists in the given array then returns the index of first occurrence,
+	 *  otherwise returns negative value of (index + 1) where index is the one
+	 *   at which the given number should have been
+	 */
+	public static int binaryIndexOf(int[] ar, int number) {
+		int left = 0;
+		int right = ar.length - 1;
+		int middle = ar.length / 2;
+		while (left <= right && ar[middle] != number) {
+			if (ar[middle] < number) {
+				left = middle + 1; //looking for the number will be in right part of the array
+			} else {
+				right = middle - 1; //looking for the number will be in the left part of the array
+			}
+			middle = (left + right) / 2;
+		}
+		return left > right ? -1 : middle;
+		//TODO
+		// fix the code for performing the method in accordance with the above definition (see the tests)
+	}
 }
--- src/telran/numbers/tests/ArrayIntTests.java
+++ src/telran/numbers/tests/ArrayIntTests.java
@@ -2,12 +2,17 @@ package telran.numbers.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 import telran.numbers.ArrayInt;
 
 class ArrayIntTests {
 
+	private static final int SMALL_N_NUMBERS = 100;
+	private static final int LARGE_N_NUMBERS = 1_000_000;
+	private static final int N_RUNS = 10000;
 	@Test
 	void referenceTests() {
 		int ar[] = {1 ,2 ,3, 4};
@@ -45,6 +50,65 @@ class ArrayIntTests {
 		assertEquals(1, ArrayInt.indexOf(ar, 2));
 		assertEquals(0, ArrayInt.indexOf(ar, 1));
 		assertEquals(-1, ArrayInt.indexOf(ar, 10));
+	}
+	@Test
+	void sortSmallTest() {
+		int ar[] = new int[SMALL_N_NUMBERS];
+		fillRandomNumbers(ar);
+		ArrayInt.sort(ar);
+		runSortTest(ar);
+	}
+	@Test
+	void sortLargeTest() {
+		int ar[] = new int[LARGE_N_NUMBERS];
+		fillRandomNumbers(ar);
+		Arrays.sort(ar); //sorting with O[N*LogN]
+		ArrayInt.sort(ar);// bubble sorting with optimization of the HW #4
+		runSortTest(ar);
+	}
+	@Test
+	void binarySearchTest() {
+		int ar[] = {1, 5, 100, 200, 350, 500};
+		assertEquals(0, ArrayInt.binaryIndexOf(ar, 1));
+		assertEquals(2, ArrayInt.binaryIndexOf(ar, 100));
+		assertEquals(5, ArrayInt.binaryIndexOf(ar, 500));
+		assertTrue(ArrayInt.binaryIndexOf(ar, 150) < 0);
+		//Tests for HW #4
+		assertEquals(-1, ArrayInt.binaryIndexOf(ar, -10));
+		assertEquals(-4, ArrayInt.binaryIndexOf(ar, 150));
+		assertEquals(-7, ArrayInt.binaryIndexOf(ar, 600));
+		int ar1[] = {1,1,1,1,1};
+		assertEquals(0, ArrayInt.binaryIndexOf(ar1, 1));
+		
+		
+	}
+	@Test
+	void indexOfPerformance() {
+		int ar[] = new int[LARGE_N_NUMBERS];
+		for(int i = 0; i < N_RUNS; i++) {
+			assertTrue(ArrayInt.indexOf(ar, 1) < 0);
+		}
+	}
+	@Test
+	void binaryIndexOfPerformance() {
+		int ar[] = new int[LARGE_N_NUMBERS];
+		for(int i = 0; i < N_RUNS; i++) {
+			assertTrue(ArrayInt.binaryIndexOf(ar, 1) < 0);
+		}
+	}
+	
+	
+	private void runSortTest(int[] ar) {
+		for (int i = 1; i < ar.length; i++) {
+			assertTrue(ar[i - 1] <= ar[i]);
+		}
+		
+	}
+	private void fillRandomNumbers(int[] ar) {
+		for (int i = 0; i < ar.length; i++) {
+			ar[i] = (int) (Math.random() * Integer.MAX_VALUE);
+		}
+		
 	}
 
 }
