07349064eec63d699ef250e251b0013b559288ab
Yuragranovsky <yuragranovsky@gmail.com>
2022-06-30 21:10:08 +0300

    adds CW #6 with TODO's for HW #6

---
+++ src/telran/text/Strings.java
@@ -0,0 +1,37 @@
+package telran.text;
+
+public class Strings {
+/**
+ * 
+ * @param str1 ascii string with no repeated symbols
+ * @param str2 ascii string with no repeated symbols
+ * @return array with two numbers
+ *    first - number of the symbols of the str2 that exist in str1 at the same indexes
+ *    second - number of the symbols of the str2 that exist in str1 at different indexes
+ */
+	public static int[] deepNoRepeatedCompare(String str1, String str2) {
+	//TODO
+		//Two implementation hints
+		//   first: additional helper array such that array['a'] 
+		//          is an index of the symbol 'a' in str1 or -1
+		//          example, str1 - "ctab" then array['a'] = 2;
+		// ************************************************
+		//   second: implementation based on the method indexOf
+	return null;
+}
+	/**
+	 * 
+	 * @param str1 English letters (may have repeats)
+	 * @param str2 English letters (may have repeats)
+	 * @return true if :
+	 *     (1) str2 has the same as str1 length
+	 *     (2) str2 comprises of all letters from str1
+	 */
+	public static boolean isAnagram(String str1, String str2) {
+		//TODO
+		// additional helper array such that array['a'] is the number of 'a' occurrences in str1
+		//str1 = "hello", array['l'] = 2; array['w'] = 0;
+		return false;
+		
+	}
+}
---
+++ src/telran/text/tests/StringsStandardTests.java
@@ -0,0 +1,62 @@
+package telran.text.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.UnsupportedEncodingException;
+
+import org.junit.jupiter.api.Test;
+
+class StringsStandardTests {
+
+	@Test
+	void stringArraysTest() throws UnsupportedEncodingException {
+		String strEnglish = "hello";
+		String strHebrew = "ωμεν";
+		char arCh[] = {'h', 'e', 'l','l','o'};
+		byte arBytes[] = {'h', 'e', 'l','l','o'};
+		assertArrayEquals(arCh, strEnglish.toCharArray());
+		assertArrayEquals(arBytes, strEnglish.getBytes());
+		assertEquals(5, strEnglish.getBytes().length);
+		assertEquals(4, strHebrew.toCharArray().length);
+		assertEquals(8, strHebrew.getBytes("utf-8").length);
+		
+		
+	}
+	@Test
+	void equalsStrings() {
+		String str1 = "Hello";//Java creates new array of char's and str1 is reference to the array
+		String str2 = "Hello";//Java recognizes array of char's existence and puts reference to that array
+		String str3 = new String("Hello");//Java creates new array and fills it with existed array of char's
+		assertTrue(str1 == str2);
+		assertFalse(str1 == str3);
+		assertTrue(str1.equals(str3));
+		assertEquals(str1, str3);
+	}
+	@Test
+	void comparingStrings() {
+		String str1 = "123456789";
+		String str2 = "ab";
+		String str3 = "Ab";
+		String str4 = "aB";
+		assertTrue(str1.compareTo(str2) < 0);
+		assertEquals('1' - 'a', str1.compareTo(str2));
+		assertTrue(str2.compareTo(str3) > 0);
+		assertEquals(0, str2.compareToIgnoreCase(str4));
+		assertEquals(0, str2.compareTo(str4.toLowerCase()));
+		
+	}
+	@Test
+	void indexOfTests() {
+		String str = "Hello";
+		assertEquals(2, str.indexOf('l'));
+		assertEquals(3, str.lastIndexOf('l'));
+		assertEquals(-1, str.indexOf('w'));
+	}
+	@Test
+	void formatTest() {
+		String strExpected = "a=5";
+		int b = 10;
+		assertEquals(strExpected, String.format("a=%d", b / 2));
+	}
+
+}
